557db01fb056b55f847a76edc8ea9bef68ca689d
Andrew Motko <andrew.motko@gmail.com>
2024-10-23 11:04:24 +0300

    add: root $ redux

---
+++ src/redux/campers/operations.js
@@ -0,0 +1,30 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+axios.defaults.baseURL = "https://66b1f8e71ca8ad33d4f5f63e.mockapi.io";
+
+// Асинхронний екшен для отримання всіх кемперів
+export const getCampers = createAsyncThunk(
+  "campers/getAll",
+  async (_, { rejectWithValue }) => {
+    try {
+      const { data } = await axios.get("/campers");
+      return data;
+    } catch (e) {
+      return rejectWithValue(e.message);
+    }
+  }
+);
+
+// Асинхронний екшен для отримання кемпера за ID
+export const getCamperById = createAsyncThunk(
+  "campers/getCamperById",
+  async (camperId, { rejectWithValue }) => {
+    try {
+      const { data } = await axios.get(`/campers/${camperId}`);
+      return data;
+    } catch (e) {
+      return rejectWithValue(e.message);
+    }
+  }
+);
---
+++ src/redux/store.js
@@ -0,0 +1,39 @@
+import { configureStore } from "@reduxjs/toolkit";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+import { campersReducer } from "@redux/campers/slice";
+import { filtersReducer } from "@redux/filters/slice";
+import { favoritesReducer } from "@redux/favorites/slice";
+
+// Імпорт ред'юсерів:
+const persistConfig = {
+  key: "favoriteCampers",
+  storage,
+};
+
+// Конфігурація для збереження стану:
+export const store = configureStore({
+  reducer: {
+    campers: campersReducer,
+    filters: filtersReducer,
+    favorites: persistReducer(persistConfig, favoritesReducer),
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);
